277597cada4200c3f57949538471591f2689fc22
13badjei <13badjei@gmail.com>
2020-03-16 16:45:48 +0000

    My first Unity project on Github

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private int hitNumber;
+
+    void OnCollisionEnter(Collision other)
+    {
+     	if (other.transform.CompareTag("bullet")){
+	    //If the comparision is true, we increase the hit number.
+	    hitNumber++;
+	}
+	if (hitNumber == 3){
+	    Destroy(gameObject);
+	}   
+    }
+}
---
+++ Assets/PlayerDamage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class PlayerDamage : MonoBehaviour
+{
+    // Use this to reference the text in the canvas
+    public Text healthPanel;
+    //Sets default health to 100
+    public int health = 100;
+    private void Start()
+    {
+     	//Sets the health text at the start, we pass 0 as we don't want to remove health.   
+	ApplyDamage(0);
+    }
+    void ApplyDamage(int damage){
+	//Checks we have attached a health bar and our heealth is greater than 0
+	if (healthPanel != null && health > 0){
+	    //Stores the current health and subtracts the damage value
+	    health = health - damage;
+	    //Sets the text on our panel.
+	    healthPanel.text = health.ToString();
+	}        
+    }
+}
